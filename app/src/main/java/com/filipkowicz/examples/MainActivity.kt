02925e4841bd729ba9daa5096f25ce5f82dc3938
Michal Filipek <filipkowicz@gmail.com>
2019-11-18 13:57:11 +0100

    empty project

---
+++ app/src/main/java/com/filipkowicz/examples/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.filipkowicz.examples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.filipkowicz.examples.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}

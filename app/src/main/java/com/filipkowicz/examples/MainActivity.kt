c3aed83a8da215cdc69dd5a038ff3f154f3eab2d
Michal Filipek <filipkowicz@gmail.com>
2019-12-22 10:43:31 +0100

    translucent systembar

--- app/src/main/java/com/filipkowicz/examples/MainActivity.kt
+++ app/src/main/java/com/filipkowicz/examples/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.filipkowicz.examples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.filipkowicz.examples.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
